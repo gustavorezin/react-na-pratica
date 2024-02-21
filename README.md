364085ea98f801e900b4fa63e264bb84ec2e37d7
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2024-02-21 16:11:25 -0300

    Finalizando aula 01 react-na-pratica

--- README.md
+++ README.md
@@ -9,3 +9,25 @@
 - npm i -D tailwindcss postcss autoprefixer
 - npx tailwindcss init -p
 - configurar `tailwind.config.js` e `index.css`
+
+- npm i tailwind-variants tailwind-merge
+
+## Ícones
+
+- npm i lucide-react
+
+## Radix UI
+
+- npm i @radix-ui/react-select
+
+## JSON Server
+
+- npm i json-server@0.17.4 -D
+
+## React Query
+
+- npm i @tanstack/react-query
+
+## React Router Dom
+
+- npm i react-router-dom localforage match-sorter sort-by
--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,144 @@
+import { FileDown, Filter, MoreHorizontal, Plus, Search } from "lucide-react";
+import { Header } from "./components/header";
+import { Tabs } from "./components/tabs";
+import { Button } from "./components/ui/button";
+import { Control, Input } from "./components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./components/ui/table";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { Pagination } from "./components/pagination";
+import { useSearchParams } from "react-router-dom";
+import { useState } from "react";
+
+export interface TagResponse {
+  first: number;
+  prev: number | null;
+  next: number;
+  last: number;
+  pages: number;
+  items: number;
+  data: Tag[];
+}
+
+export interface Tag {
+  title: string;
+  amountOfVideos: number;
+  id: string;
+}
+
 export function App() {
-  return <h1>Hello World</h1>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const urlFilter = searchParams.get("filter") ?? "";
+  const [filter, setFilter] = useState(urlFilter);
+
+  const page = searchParams.get("page") ? Number(searchParams.get("page")) : 1;
+
+  const { data: tagsReponse, isLoading } = useQuery<TagResponse>({
+    queryKey: ["get-tags", urlFilter, page],
+    queryFn: async () => {
+      const response = await fetch(
+        `http://localhost:3333/tags?_page=${page}&_per_page=10&title=${urlFilter}`
+      );
+      const data = await response.json();
+      return data;
+    },
+    placeholderData: keepPreviousData,
+    //staleTime: 1000 * 60,
+  });
+
+  function handleFilter() {
+    setSearchParams((params) => {
+      params.set("page", "1");
+      params.set("filter", filter);
+      return params;
+    });
+  }
+
+  if (isLoading) {
+    return null;
+  }
+
+  return (
+    <div className="py-10 space-y-8">
+      <div>
+        <Header />
+        <Tabs />
+      </div>
+      <main className="max-w-6xl mx-auto space-y-5">
+        <div className="flex items-center gap-3">
+          <h1 className="text-xl font-bold">Tags</h1>
+          <Button variant="primary">
+            <Plus className="size-3" />
+            Create new
+          </Button>
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <Input variant="filter">
+              <Search className="size-3" />
+              <Control
+                placeholder="Search tags..."
+                onChange={(e) => setFilter(e.target.value)}
+                value={filter}
+              />
+            </Input>
+            <Button onClick={handleFilter}>
+              <Filter className="size-3" />
+              Filter
+            </Button>
+          </div>
+          <Button>
+            <FileDown className="size-3" />
+            Export
+          </Button>
+        </div>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead></TableHead>
+              <TableHead>Tag</TableHead>
+              <TableHead>Amount of videos</TableHead>
+              <TableHead></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {tagsReponse?.data.map((tag) => {
+              return (
+                <TableRow key={tag.id}>
+                  <TableCell></TableCell>
+                  <TableCell>
+                    <div className="flex flex-col">
+                      <span className="font-medium">{tag.title}</span>
+                      <span className="text-xs text-zinc-500">{tag.id}</span>
+                    </div>
+                  </TableCell>
+                  <TableCell className="text-zinc-300">
+                    {tag.amountOfVideos} vídeo(s)
+                  </TableCell>
+                  <TableCell className="text-right">
+                    <Button size="icon">
+                      <MoreHorizontal className="size-4" />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+        {tagsReponse && (
+          <Pagination
+            pages={tagsReponse.pages}
+            items={tagsReponse.items}
+            page={page}
+          />
+        )}
+      </main>
+    </div>
+  );
 }
